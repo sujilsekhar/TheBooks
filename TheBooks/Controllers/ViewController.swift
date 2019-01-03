2eeb8da11aa690b8b4f26886034b550b20e2e988
Sujil Chandrasekharan <Sujil.Chandrasekharan022@harman.com>
2019-01-03 17:49:19 +0530

    Initial UI elements and framework

---
+++ TheBooks/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheBooks
+//
+//  Created by Sujil Chandrasekharan on 03/01/19.
+//  Copyright © 2019 Sujil Chandrasekharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
